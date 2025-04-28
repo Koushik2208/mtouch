541053a4b595c0494e073625b6c003cfceea0477
Koushik2208 <gorrekoushik@gmail.com>
2025-04-28 15:52:22 +0530

    mtouchtask

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 // import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 
-// export const getServerSideProps = (async (context) => {
-//   const { id } = await context.query;
+// export const getServerSideProps = (async ({params}) => {
+//   const { id } = await params;
 //   // Fetch data from external API
 //   const res = await fetch(`https://fakestoreapi.com/products/${id}`);
 //   const product: Product = await res.json();
@@ -19,10 +19,20 @@
 //   );
 // }
 
+import Image from "next/image";
 import React from "react";
 
-const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+const ProductDetails = async ({ params }: { params: { id: string } }) => {
+  const { id } = await params;
+  const detail = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const product = await detail.json();
+  console.log(product);
+  return (
+    <div>
+      <h1>{product.title}</h1>
+      <Image src={product.image} alt="product image" width={300} height={300} />
+    </div>
+  );
 };
 
 export default ProductDetails;
